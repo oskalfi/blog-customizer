0746941e001c93aa3598fe82b2cc4379120f0dcd
Adam Stadnikov <stadnikov.adam@gmail.com>
2025-04-12 13:10:27 +0300

    version 1.0.0

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,123 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
+import { SyntheticEvent, useState } from 'react';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	toggleIsOpenState: Function;
+	formParams: typeof defaultArticleState;
+	setFormParams: Function;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	toggleIsOpenState,
+	formParams,
+	setFormParams,
+}: ArticleParamsFormProps) => {
+	const [changedFormParams, setChangedFormParams] = useState(formParams);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					toggleIsOpenState(!isOpen);
+				}}
+			/>
+			<aside
+				className={
+					isOpen
+						? clsx(styles.container, styles.container_open)
+						: styles.container
+				}>
 				<form className={styles.form}>
+					<Select
+						title={'Шрифт'}
+						selected={changedFormParams.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(e) => {
+							setChangedFormParams({
+								...changedFormParams,
+								fontFamilyOption: e,
+							});
+						}}
+					/>
+					<RadioGroup
+						title={'Размер шрифта'}
+						selected={changedFormParams.fontSizeOption}
+						options={fontSizeOptions}
+						name={'font-size-radio'}
+						onChange={(e) => {
+							setChangedFormParams({ ...changedFormParams, fontSizeOption: e });
+						}}
+					/>
+					<Select
+						title={'Цвет шрифта'}
+						selected={changedFormParams.fontColor}
+						options={fontColors}
+						onChange={(e) => {
+							setChangedFormParams({
+								...changedFormParams,
+								fontColor: e,
+							});
+						}}
+					/>
+					<Separator />
+					<Select
+						title={'Цвет фона'}
+						selected={changedFormParams.backgroundColor}
+						options={backgroundColors}
+						onChange={(e) => {
+							setChangedFormParams({
+								...changedFormParams,
+								backgroundColor: e,
+							});
+						}}
+					/>
+					<Select
+						title={'Ширина контента'}
+						selected={changedFormParams.contentWidth}
+						options={contentWidthArr}
+						onChange={(e) => {
+							setChangedFormParams({
+								...changedFormParams,
+								contentWidth: e,
+							});
+						}}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={(e: SyntheticEvent) => {
+								e.preventDefault();
+								setFormParams(defaultArticleState);
+								setChangedFormParams(defaultArticleState);
+							}}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={(e: SyntheticEvent) => {
+								e.preventDefault();
+								setFormParams(changedFormParams);
+							}}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,27 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [paramsFormIsOpen, setParamsFormIsOpen] = useState(false);
+	const [currentArticleState, setCurrentArticleState] =
+		useState(defaultArticleState);
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentArticleState.fontFamilyOption.value,
+					'--font-size': currentArticleState.fontSizeOption.value,
+					'--font-color': currentArticleState.fontColor.value,
+					'--container-width': currentArticleState.contentWidth.value,
+					'--bg-color': currentArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={paramsFormIsOpen}
+				toggleIsOpenState={setParamsFormIsOpen}
+				formParams={currentArticleState}
+				setFormParams={setCurrentArticleState}
+			/>
 			<Article />
 		</main>
 	);
